94d0c492ad386cebaedf873711bef20190172e83
Amanda Ramcharran <amanda_ramcharran@tjx.com>
2023-03-09 14:01:34 +0000

    Updated app message

--- labs/continuous-integration/src/HelloWorld/Program.cs
+++ labs/continuous-integration/src/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World! My name is Amanda");
         if (args.Length == 1)
         {
             int bedtimeHour = 22;
