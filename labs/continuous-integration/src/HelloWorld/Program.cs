c4fb2d4b0313f7dbf20f7bab141c5041510ac992
Amanda Ramcharran <amanda_ramcharran@tjx.com>
2023-03-09 14:22:38 +0000

    Updated app message with a new line

--- labs/continuous-integration/src/HelloWorld/Program.cs
+++ labs/continuous-integration/src/HelloWorld/Program.cs
@@ -7,10 +7,11 @@ public class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! My name is Amanda Ramcharran");
+        Console.WriteLine("Hello World! check repo for changes");
         if (args.Length == 1)
         {
             int bedtimeHour = 22;
-            int.TryParse(args[1], out bedtimeHour);
+            int.TryParse(args[0], out bedtimeHour);
             var today = DateTime.Today;
             var bedtime = new DateTime(today.Year, today.Month, today.Day, bedtimeHour, 0, 0);
             var duration = bedtime.Subtract(DateTime.Now);
