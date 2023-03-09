f8919251a30c0f66b1cba4e740425038a07202e2
Payton <payton@example.com>
2023-03-09 13:57:42 +0000

    Updated app message

--- labs/continuous-integration/src/HelloWorld/Program.cs
+++ labs/continuous-integration/src/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello Class!");
         if (args.Length == 1)
         {
             int bedtimeHour = 22;
